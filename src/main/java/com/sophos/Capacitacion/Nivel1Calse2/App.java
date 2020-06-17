41ccda65d6954203a8ecfc695dc649c56d29b158
andres253901 <andrespirazan@Andress-MacBook-Pro.local>
2020-06-17 08:17:17 -0500

    primer commit clase 2

---
+++ src/main/java/com/sophos/Capacitacion/Nivel1Calse2/App.java
@@ -0,0 +1,14 @@
+package com.sophos.Capacitacion.Nivel1Calse2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
